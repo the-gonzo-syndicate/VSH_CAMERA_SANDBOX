caab9234b71104365dc144ef51415473dadb52a0
AndrewStudenic <36557385+AndrewStudenic@users.noreply.github.com>
2019-04-07 23:31:33 -0700

    Initial Commit

---
+++ VSH_CAMERA_SANDBOX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VSH_CAMERA_SANDBOX
+//
+//  Created by Andrew on 4/7/19.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
